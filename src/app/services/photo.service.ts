aef869822b469796d6a04d1e1675fabf5e1bb57b
Viktor Bonazza Charlanti <105568185+ViiKDev@users.noreply.github.com>
2022-06-11 08:23:59 -0300

    Add files via upload

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,29 @@
+import { UserPhoto } from './../models/UserPhoto.model';
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  public photos: UserPhoto[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 50
+    });
+
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,21 @@
+import { PhotoService } from './../services/photo.service';
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public photoService: PhotoService) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+
+
+}
